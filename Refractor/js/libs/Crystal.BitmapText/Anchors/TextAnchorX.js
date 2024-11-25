f77279b5c56e8d0b60cecaef0b7f39660ea38e3b
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2024-11-25 21:48:24 +0800

    e

--- Refractor/js/libs/Crystal.BitmapText/Anchors/TextAnchorX.js
+++
@@ -1,6 +0,0 @@
-class TextAnchorX
-{
-    static get Left () { return 0; }
-    static get Center () { return 1; }
-    static get Right () { return 2; }
-}
--- Refractor/js/libs/Crystal.BitmapText/Anchors/TextAnchorY.js
+++
@@ -1,6 +0,0 @@
-class TextAnchorY
-{
-    static get Top () { return 0; }
-    static get Middle () { return 1; }
-    static get Bottom () { return 2; }
-}
