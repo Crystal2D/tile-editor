8fd3262fd7c8e257bd7f34336951543a2b258059
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2025-02-02 18:21:52 +0800

    Update MapperInput.js

--- Refractor/js/libs/Refractor.Input/MapperInput.js
+++ Refractor/js/libs/Refractor.Input/MapperInput.js
@@ -2,6 +2,10 @@ class MapperInput extends GameBehavior
 {
     #inputHandler = null;
     #background = null;
+    #creationRect = null;
+    #creationRenderer = null;
+    #createStart = null;
+    #createEnd = null;
 
     Start ()
     {
@@ -18,6 +22,83 @@ class MapperInput extends GameBehavior
         }
 
         this.#background.GetComponent("RectRenderer").sortingLayer = bgLayerID;
+    }
+
+    async Update ()
+    {
+        return;
+
+        if (InputManager.GetKeyDown("left"))
+        {
+            // bad idea to place await in Update loop
+            await SceneInjector.GameObject({
+                name: "AAAAAAAAAAAAAAAAAAA",
+                id: 1111,
+                components: [
+                    {
+                        type: "RectRenderer"
+                    }
+                ]
+            });
+
+            this.#creationRect = SceneBank.FindByID(1111);
+            this.#creationRenderer = this.#creationRect.GetComponent("RectRenderer");
+            this.#createStart = this.#inputHandler.mousePos;
+        }
+
+        // if (this.#createStart == null)
+        // {
+        //     if (InputManager.isMouseOver && !this.#selectionRenderer.color.Equals(Color.white)) this.#selectionRenderer.color = Color.white;
+        //     else if (!InputManager.isMouseOver) this.#selectionRenderer.color = new Color(0, 0, 0, 0);
+
+        //     const gridSize = Vector2.Add(grid.cellSize, grid.cellGap);
+
+        //     if (!this.#selectionRect.transform.scale.Equals(gridSize)) this.#selectionRect.transform.scale = gridSize;
+            
+        //     this.#selectionRect.transform.position = this.#inputHandler.mousePosSnapped;
+        // }
+
+        if (this.#createStart != null && InputManager.GetKey("left"))
+        {
+            if (!this.#creationRenderer.color.Equals(new Color(0, 1, 1))) this.#creationRenderer.color = new Color(0, 1, 1);
+
+            this.#createEnd = this.#inputHandler.mousePos;
+
+            const rect = new Rect();
+
+            if (this.#createStart.x < this.#createEnd.x)
+            {
+                rect.xMin = this.#createStart.x;
+                rect.xMax = this.#createEnd.x;
+            }
+            else
+            {
+                rect.xMin = this.#createEnd.x;
+                rect.xMax = this.#createStart.x;
+            }
+
+            if (this.#createStart.y < this.#createEnd.y)
+            {
+                rect.yMin = this.#createStart.y;
+                rect.yMax = this.#createEnd.y;
+            }
+            else
+            {
+                rect.yMin = this.#createEnd.y;
+                rect.yMax = this.#createStart.y;
+            }
+
+            this.#creationRect.transform.position = rect.center;
+            this.#creationRect.transform.scale = rect.size;
+        }
+
+        if (this.#createStart != null && InputManager.GetKeyUp("left"))
+        {
+            this.#creationRect = null;
+            this.#creationRenderer = null;
+            this.#createStart = null;
+            this.#createEnd = null;
+        }
     }
 
     SetBounds (size)
