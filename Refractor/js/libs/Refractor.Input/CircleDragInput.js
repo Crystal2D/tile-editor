a8ab45e1b3ca639c0588ae7bd58c7ff84eae2696
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2025-02-18 22:50:07 +0800

    Update CircleDragInput.js

--- Refractor/js/libs/Refractor.Input/CircleDragInput.js
+++ Refractor/js/libs/Refractor.Input/CircleDragInput.js
@@ -1,10 +1,18 @@
 class CircleDragInput extends GameBehavior
 {
+    #mouseOver = false;
+    #moving = false;
+
     #inputHandler = null;
     #mapperInput = null;
-    #renderer = null;
     #cam = null;
-    #rect = null;
+    #clickedPos = null;
+    #clickOffset = null;
+
+    get isMouseOver ()
+    {
+        return this.#mouseOver;
+    }
 
     Start ()
     {
@@ -12,16 +20,81 @@ class CircleDragInput extends GameBehavior
 
         this.#inputHandler = handlers.GetComponent("InputHandler");
         this.#mapperInput = handlers.GetComponent("MapperInput");
-        this.#renderer = this.GetComponent("CircleRenderer");
         this.#cam = GameObject.Find("camera").GetComponent("Camera");
     }
 
     Update ()
     {
         const mousePos = this.#inputHandler.mousePos;
+        const hovered = Vector2.Distance(mousePos, this.transform.position) <= (this.#cam.orthographicSize * 0.15 * (4 / 40));
 
-        if (Vector2.Distance(mousePos, this.transform.position) > (this.#cam.orthographicSize * 0.15 * (4 / 40))) return;
+        if (this.#mouseOver !== hovered)
+        {
+            if (hovered) this.#OnMouseEnter();
+            else this.#OnMouseExit();
 
+            this.#mouseOver = hovered;
+        }
+
+        if (hovered && InputManager.GetKeyDown("left"))
+        {
+            this.#OnMouseDown();
+
+            this.#clickedPos = mousePos;
+            this.#clickOffset = Vector2.Subtract(this.transform.position, this.#clickedPos);
+        }
+
+        if (this.#clickOffset == null) return;
+        
+        if (!this.#clickedPos.Equals(mousePos)) this.#moving = true;
+
+        if (InputManager.GetKeyUp("left"))
+        {
+            this.#OnMouseUp();
+            
+            this.#moving = false;
+            this.#clickedPos = null;
+            this.#clickOffset = null;
+        }
+
+        if (this.#moving)
+        {
+            const newPos = Vector2.Add(mousePos, this.#clickOffset);
+
+            this.#OnDrag(newPos);
+        }
+    }
+
+    #OnMouseEnter ()
+    {
         this.#mapperInput.SetCursor("move");
+    }
+
+    #OnMouseExit ()
+    {
+        this.#mapperInput.SetCursor("");
+    }
+
+    #OnMouseDown ()
+    {
+        this.#mapperInput.cursorLocked = true;
+    }
+
+    #OnMouseUp ()
+    {
+        this.#mapperInput.cursorLocked = false;
+
+        this.#mapperInput.SetCursor("");
+    }
+
+    #OnDrag (pos)
+    {
+        pos = Vector2.Clamp(
+            pos,
+            this.#mapperInput.focused.rect.min,
+            this.#mapperInput.focused.rect.max,
+        );
+
+        this.transform.position = pos;
     }
 }
